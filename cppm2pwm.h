dfa5121f7ea2a59378d61757f8c1f9884e7945e8
BlackJohnny <blackjohnny@gmail.com>
2015-04-14 22:40:21 +0300

    License message added

--- cppm2pwm.h
+++ cppm2pwm.h
@@ -1,3 +1,18 @@
+/*
+ * This file is part of cppm2pwm.
+ *
+ * cppm2pwm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * cppm2pwm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cppm2pwm.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef Ccppm2pwm_h
 #define Ccppm2pwm_h
 
